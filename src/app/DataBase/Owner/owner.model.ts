d460ebed59454df5d2a32df1786fdaaf54bd4bf4
ubaidurrehman87 <ubaidurrehman225786@gmail.com>
2019-11-14 19:18:51 +0500

    update

---
+++ src/app/DataBase/Owner/owner.model.ts
@@ -0,0 +1,7 @@
+export class Owner {
+    _id : string;
+    firstname : string;
+    lastname : string;
+    email : string;
+    cnic : string;
+}
---
+++ src/app/services/Tenant/rent.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Rent } from '../../models/tenant/rent.model';
+import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/toPromise';
+// import 'rxjs/add/operator/map';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentService {
+  selectedRent : Rent;
+  rents : Rent[];
+  readonly baseURL = "http://localhost:3000/rent";
+  constructor(private http: HttpClient) { }
+
+  postRent(rent: Rent){
+    return   this.http.post(this.baseURL,rent);
+  }
+  getRent(){
+    return  this.http.get(this.baseURL);
+  }
+  getOwneById(rent : Rent){
+    return  this.http.get(this.baseURL+'/${rent._id}');
+  }
+  editRent(rent: Rent){
+    return  this.http.put(this.baseURL+'/${rent._id}',rent);
+  }
+  deleteRent(rent : Rent){
+    return  this.http.delete(this.baseURL+'/${rent._id}');
+  }
+}
